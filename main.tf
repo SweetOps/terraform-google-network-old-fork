a849ef98c04d43ea9971a23b9913dc933e7a93f9
SweetOps <vladimir.fess@gmail.com>
2017-10-15 16:43:30 +0300

    add compact

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ data "google_client_config" "default" {}
 resource "google_compute_network" "default" {
   name                    = "${var.name}"
   auto_create_subnetworks = "${var.auto_create_subnetworks}"
-  project                 = "${length(var.project) > 0 ? var.project : data.google_client_config.default.project}"
+  project                 = "${length(compact(var.project)) > 0 ? var.project : data.google_client_config.default.project}"
   description             = "${var.description}"
 }
